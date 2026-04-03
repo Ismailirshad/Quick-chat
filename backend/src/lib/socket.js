ba4155920610c812ed82026ad704094d945db05d
Ismail Irshad <irshadsha164@gmail.com>
2026-04-03 20:12:52 +0530

    fix render socket issue

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,43 +1,52 @@
-import {Server} from 'socket.io';
+import { Server } from "socket.io";
 import http from "http";
-import express from 'express';
-import { ENV } from './env.js';
-import { socketAuthMiddleware } from '../middleware/socketAuthMiddleware.js';
+import express from "express";
+import { ENV } from "./env.js";
+import { socketAuthMiddleware } from "../middleware/socketAuthMiddleware.js";
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
+
+// const io = new Server(server, {
+//     cors: {
+//         origin: [ENV.CLIENT_URL],
+//         credentials: true,
+//     },
+// })
 
 const io = new Server(server, {
-    cors: {
-        origin: [ENV.CLIENT_URL],
-        credentials: true,
-    },
-})
-// apply authentication middleware to all socket connections 
-io.use(socketAuthMiddleware)
+  cors: {
+    origin: ["http://localhost:5173", "https://quickchat.ismailirshad.in"],
+    credentials: true,
+  },
+  transports: ["websocket", "polling"],
+});
 
-// we will use this fucntion to check if the user is online or not 
+// apply authentication middleware to all socket connections
+io.use(socketAuthMiddleware);
+
+// we will use this fucntion to check if the user is online or not
 export function getReceiverSocketId(userId) {
-    return userSocketMap[userId]
+  return userSocketMap[userId];
 }
 
-// this is for storing online users 
+// this is for storing online users
 const userSocketMap = {};
 
 io.on("connection", (socket) => {
-    console.log("A user connected", socket.user.fullName);
+  console.log("A user connected", socket.user.fullName);
 
-    const userId = socket.user._id.toString();
-    userSocketMap[userId] = socket.id;
+  const userId = socket.user._id.toString();
+  userSocketMap[userId] = socket.id;
 
-    // io.emit() is used to send event to all connected  clients
-    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+  // io.emit() is used to send event to all connected  clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect", () => {
-        console.log("A user disconnected", socket.user.fullName)
-        delete userSocketMap[userId]
-        io.emit("getOnlineUsers", Object.keys(userSocketMap))
-    })
-})
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.user.fullName);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
-export {io, app, server}
+export { io, app, server };
